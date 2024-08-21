9fb78248f62a08c65ef9cd022e2ce6e821a5b3dd
donado10 <adamscoulibaly10@gmail.com>
2024-08-21 20:13:37 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#C73B0F",
+        "rose-900": "#260F08",
+        "rose-500": "#87635A",
+        "rose-400": "#AD8A85",
+        "rose-300": "#CAAFA7",
+        "rose-100": "#F5EEEC",
+        "rose-50": "#FCF8F6",
+        green: "#1EA575",
+      },
+      fontFamily: {
+        redHat: ["Red Hat Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
